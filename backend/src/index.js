30237f2a5b07ec0f94bb0186a82f7ea498879991
Estelle Bolay <152796964+Exbly00@users.noreply.github.com>
2025-10-19 12:19:50 +0200

    Create and list posts

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,55 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+
+const postSchema = new mongoose.Schema({
+  latitude: Number,
+  longitude: Number,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+
+  res.json(posts);
 });
+
+app.get("/posts/:id", (req, res) => {});
+
+app.get("/users/:id/posts", (req, res) => {});
+
+app.post("/posts", (req, res) => {
+  const post = new Post({
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+  });
+
+  post.save();
+
+  res.status(201);
+  res.json(post);
+});
+
+app.patch("/posts/:id", (req, res) => {});
+
+app.delete("/posts/:id", (req, res) => {});
+
+async function start() {
+  await mongoose.connect("mongodb://localhost:27017/gtp");
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+start();
