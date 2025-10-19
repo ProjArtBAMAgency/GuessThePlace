26519d6b9cd053486502f00a5fbfb86a90f99029
Estelle Bolay <152796964+Exbly00@users.noreply.github.com>
2025-10-19 13:09:23 +0200

    Add picture upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const app = express();
 const port = 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    // 16MB limit because MongoDB
+    fileSize: 16 * 1024 * 1024,
+  },
+});
 
 const postSchema = new mongoose.Schema(
   {
     latitude: Number,
     longitude: Number,
     isValidated: Boolean,
+    picture: Buffer,
+    pictureContentType: String,
+    pictureSize: Number,
   },
   {
     timestamps: true,
   }
 );
+
+postSchema.set("toJSON", {
+  transform: (_, ret) => {
+    delete ret.picture;
+    delete ret.pictureContentType;
+    delete ret.pictureSize;
+    return ret;
+  },
+});
 
 const Post = mongoose.model("Post", postSchema);
 
@@ -55,17 +77,33 @@ app.get("/posts/:id", async (req, res) => {
 
 app.get("/users/:id/posts", (req, res) => {});
 
-app.post("/posts", async (req, res) => {
+app.post("/posts", upload.single("picture"), async (req, res) => {
   const post = new Post({
     latitude: req.body.latitude,
     longitude: req.body.longitude,
     isValidated: false,
+    picture: req.file.buffer,
+    pictureContentType: req.file.mimetype,
+    pictureSize: req.file.size,
   });
 
   await post.save();
 
   res.status(201);
   res.json(post);
+});
+
+app.get("/posts/:id/picture", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(404);
+    res.send();
+    return;
+  }
+
+  res.set("Content-Type", post.pictureContentType);
+  res.set("Content-Length", post.pictureSize);
+  res.send(post.picture);
 });
 
 app.patch("/posts/:id", async (req, res) => {
