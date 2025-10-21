fc149dd35e02144ff9b123c15fd3cb9f6d391953
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-10-21 17:08:41 +0200

    connected express app to mongodb with dotenv and Mongoose

---
+++ src/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connectDB(){
+    const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error("❌ MONGODB_URI manquant dans .env");
+  }
+
+  mongoose.set("strictQuery", true);
+
+  try {
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB connecté !");
+  } catch (err) {
+    console.error("❌ Erreur de connexion MongoDB :", err);
+    process.exit(1);
+  }
+}
