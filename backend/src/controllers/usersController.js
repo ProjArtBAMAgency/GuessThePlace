5074c0f0e30a8521f6e2232e130e848433073a35
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-11-04 10:44:15 +0100

    add creation controller and route

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,1 +1,14 @@
 // implémentation du controller usersController.js
+import User from "../models/User.js";
+
+export const createUser = async (req, res, next) => {
+    try {
+        const { pseudo, email, password_hash, is_admin } = req.body;
+        const user = new User({ pseudo, email, password_hash, is_admin });
+        await user.save();
+        res.status(201).json(user);
+    }
+    catch (error) {
+        next(error);
+    }
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,8 +3,20 @@ import mongoose from "mongoose";
 // définition du schéma pour les utilisateurs
 const userSchema = new mongoose.Schema(
     {
-        pseudo: { type: String, unique: true, required: true },
-        email: { type: String, unique: true, required: true },
+        pseudo: {
+            type: String,
+            unique: [true, "pseudo must be unique"],
+            minLength: [6, "pseudo must be at least 6 characters long"],
+            maxLength: [10, "pseudo must be at most 10 characters long"],
+            match: [/^[a-z]+$/, "pseudo must be in lowercases and not contain special characters"],
+            required: [true, "pseudo is required"]
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/, "invalid email address"]
+        },
         password_hash: { type: String, required: true },
         is_admin: { type: Boolean, default: false },
     },
