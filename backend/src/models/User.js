8a7782d479b5ffbad2fbe5851444495e83189b61
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-11-04 09:33:57 +0100

    add types

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 // définition du schéma pour les utilisateurs
 const userSchema = new mongoose.Schema(
-  {
-    pseudo: {type : String, unique: true, required: true},
-    email: {type : String, unique: true, required: true},
-    password_hash: { type: String, required: true },
-    is_admin: {type: Boolean, default: false},
-  },
-  {
-    timestamps: true,
-  }
+    {
+        pseudo: { type: String, unique: true, required: true },
+        email: { type: String, unique: true, required: true },
+        password_hash: { type: String, required: true },
+        is_admin: { type: Boolean, default: false },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 // à partir du schéma, on crée le modèle Mongoose
