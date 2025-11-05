17d8fed55fe620754044ac98bb8d8a98528cf541
Maximilien Maret <maret.maximilien@gmail.com>
2025-11-05 15:16:53 +0100

    chagement index.js - nom guesses

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import authentificationRouter from "./authentification/index.js";
 import postsRouter from "./posts/index.js";
 import usersRouter from "./users/index.js";
-import guessRouter from "./guesses/index.js";
+import guessesRouter from "./guesses/index.js";
 
 // Ici, on importe les routes de chaque module et on les assembles 
 // dans le routeur principal. Celui-ci sera utilisé ensuite dans le fichier app.js.
@@ -12,6 +12,6 @@ const router = express.Router();
 router.use("/authentification", authentificationRouter);
 router.use("/posts", postsRouter);
 router.use("/users", usersRouter);
-router.use("/guess", guessRouter);
+router.use("/guesses", guessesRouter);
 
 export default router;
