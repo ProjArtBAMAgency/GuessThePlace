082f4849adc77a808fda28854b3622e7055ef93b
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-11-17 13:14:07 +0100

    teams model finished

--- backend/src/models/Teams.js
+++ backend/src/models/Teams.js
@@ -2,5 +2,11 @@ import mongoose from "mongoose";
 
 const teamSchema = new mongoose.Schema(
     {
-         teamId :{type : Number}
-})
+         teamId :{type : Number, required : true},
+         name : {type : String, required : true},
+         color : String,
+}, {timestamps : true}
+);
+
+const Teams = mongoose.model("Teams", teamSchema);
+export default Teams;
