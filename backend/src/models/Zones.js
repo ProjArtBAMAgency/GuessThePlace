c0103edd18b437a25164e9ae4a69a3792b563285
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-11-19 15:18:13 +0100

    début du schéma pour Zones

--- backend/src/models/Zones.js
+++ backend/src/models/Zones.js
@@ -1,2 +1,13 @@
 import mongoose from "mongoose";
 
+
+const zoneSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            unique: [true, "zone name must be unique"],
+            required: [true, "zone name is required"]
+        },
+        zone: {
+            type: Object,
+    })
