467f06587298274127c0d17579b8062ef209c9c1
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-11-19 14:05:56 +0100

    backend is now serving the front

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,27 @@
 import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import v1Router from "./routes/v1/index.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticDir = path.join(__dirname, "..", "..", "frontend", "dist");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(staticDir));
 
 app.use("/api/v1", v1Router);
 
-app.get("/", (req, res) => res.send("API GuessThePlace"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(staticDir, "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
