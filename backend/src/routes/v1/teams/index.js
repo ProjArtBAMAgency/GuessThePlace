81d7cba5eaeda7708abc876e260ea0bbfee3b22f
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-11-19 14:40:36 +0100

    route for teams is done

--- backend/src/routes/v1/teams/index.js
+++ backend/src/routes/v1/teams/index.js
@@ -2,8 +2,16 @@ import express from "express";
 
 import {
   getTeams,
-  getTeamByIdyId,
+  getTeamById,
   createTeam,
   getTeamsLeaderboard
 } from "../../../controllers/teamsController.js";
 
+const router = express.Router();
+
+router.get("/", getTeams);
+router.get("/:id", getTeamById);
+router.post("/", createTeam);
+router.get("/leaderboard", getTeamsLeaderboard);
+
+export default router;  
