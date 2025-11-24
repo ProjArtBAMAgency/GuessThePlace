201bd279380ee62c26b615f27dcccaae75061a54
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-11-24 11:40:37 +0100

    navbar done

--- frontend/src/components/TheNavBar.vue
+++ frontend/src/components/TheNavBar.vue
@@ -1,15 +1,139 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 
-
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/camera">Camera</RouterLink>
-    <RouterLink to="/map">Map</RouterLink>
-    <RouterLink to="/ranking">Ranking</RouterLink>
-    <RouterLink to="/profile">Profile</RouterLink>
-  </nav>
+  <div
+    class="fixed bottom-0 left-0 z-50 w-full h-16 bg-neutral-primary-soft border-t border-default"
+  >
+    <div class="grid h-full max-w-lg grid-cols-5 mx-auto font-medium">
+      <!-- Home -->
+      <RouterLink
+        to="/"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-house-icon lucide-house"
+        >
+          <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+          <path
+            d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+          />
+        </svg>
 
+        <span class="text-sm text-body group-hover:text-fg-brand">Home</span>
+      </RouterLink>
+
+      <!-- Camera -->
+      <RouterLink
+        to="/camera"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-camera-icon lucide-camera"
+        >
+          <path
+            d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z"
+          />
+          <circle cx="12" cy="13" r="3" />
+        </svg>
+        <span class="text-sm text-body group-hover:text-fg-brand">Camera</span>
+      </RouterLink>
+
+      <!-- Map -->
+      <RouterLink
+        to="/map"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-map-icon lucide-map"
+        >
+          <path
+            d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"
+          />
+          <path d="M15 5.764v15" />
+          <path d="M9 3.236v15" />
+        </svg>
+
+        <span class="text-sm text-body group-hover:text-fg-brand">Map</span>
+      </RouterLink>
+
+      <!-- Ranking -->
+      <RouterLink
+        to="/ranking"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-trophy-icon lucide-trophy"
+        >
+          <path d="M10 14.66v1.626a2 2 0 0 1-.976 1.696A5 5 0 0 0 7 21.978" />
+          <path d="M14 14.66v1.626a2 2 0 0 0 .976 1.696A5 5 0 0 1 17 21.978" />
+          <path d="M18 9h1.5a1 1 0 0 0 0-5H18" />
+          <path d="M4 22h16" />
+          <path d="M6 9a6 6 0 0 0 12 0V3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1z" />
+          <path d="M6 9H4.5a1 1 0 0 1 0-5H6" />
+        </svg>
+        <span class="text-sm text-body group-hover:text-fg-brand">Ranking</span>
+      </RouterLink>
+
+      <!-- Profile -->
+      <RouterLink
+        to="/profile"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-user-icon lucide-user"
+        >
+          <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+          <circle cx="12" cy="7" r="4" />
+        </svg>
+        <span class="text-sm text-body group-hover:text-fg-brand">Profile</span>
+      </RouterLink>
+    </div>
+  </div>
 </template>
