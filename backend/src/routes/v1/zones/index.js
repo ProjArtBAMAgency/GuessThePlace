3c62bb5bcc64c42c543d9adecaa42efb8167517f
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-11-24 09:35:49 +0100

    zones route done

--- backend/src/routes/v1/zones/index.js
+++ backend/src/routes/v1/zones/index.js
@@ -1,2 +1,14 @@
 import express from 'express';
 
+
+import {
+    getZones,
+    getZoneById
+} from '../../../controllers/zonesController.js';
+
+const router = express.Router();
+
+router.get('/', getZones);
+router.get('/:id', getZoneById);
+
+export default router;
