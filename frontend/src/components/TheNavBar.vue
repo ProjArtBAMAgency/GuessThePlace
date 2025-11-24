27bb9b66e7aa949ea83549c34fac124a60bb78ac
Mathilde <62794471+heaspery@users.noreply.github.com>
2025-11-24 09:50:49 +0100

    set up component

---
+++ frontend/src/components/TheNavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/camera">Camera</RouterLink>
+    <RouterLink to="/map">Map</RouterLink>
+    <RouterLink to="/ranking">Ranking</RouterLink>
+    <RouterLink to="/profile">Profile</RouterLink>
+  </nav>
+
+</template>
