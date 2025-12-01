7ead514061e02b92e8a0964d32463ee4740c5481
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-12-01 13:07:42 +0100

    created new route through which to send the geojson data to draw the swiss map by regions

--- backend/src/routes/v1/zones/index.js
+++ backend/src/routes/v1/zones/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 
 import {
     getZones,
-    getZoneById
+    getZoneById,
+    getZonesForMap
 } from '../../../controllers/zonesController.js';
 
 const router = express.Router();
 
 router.get('/', getZones);
 router.get('/:id', getZoneById);
+router.get('/map', getZonesForMap);
 
 export default router;
