4e914740643d83c7e3a38fa2bbee0f2f6acd8e53
Mathilde <62794471+heaspery@users.noreply.github.com>
2026-01-07 17:30:58 +0100

    setup pwa

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,14 @@ import router from './router'
 import './css/style.css'
 import { store } from './store/store'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+
+app.use(router).use(store).mount('#app')
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/serviceWorker.js').catch((err) => {
+			console.error('Service worker registration failed:', err)
+		})
+	})
+}
